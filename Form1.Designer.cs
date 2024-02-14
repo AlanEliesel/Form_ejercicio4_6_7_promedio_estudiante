88b089677a322f56434a736e6a42932841b714ea
Alan <Alan@DESKTOP-TG930HR>
2024-02-14 13:40:38 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Form_ejercicio4_promedio_estudiante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_nota1 = new System.Windows.Forms.TextBox();
+            this.lbl_n1 = new System.Windows.Forms.Label();
+            this.lbl_n2 = new System.Windows.Forms.Label();
+            this.txt_nota2 = new System.Windows.Forms.TextBox();
+            this.txt_nota3 = new System.Windows.Forms.TextBox();
+            this.lbl_n4 = new System.Windows.Forms.Label();
+            this.txt_nota4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_promedio = new System.Windows.Forms.TextBox();
+            this.lbl_n3 = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.lbl_Situacion = new System.Windows.Forms.Label();
+            this.txt_situacion = new System.Windows.Forms.TextBox();
+            this.lbl_Pruba_Completiva = new System.Windows.Forms.Label();
+            this.txt_Pruba_Completiva = new System.Windows.Forms.TextBox();
+            this.lbl_Resultado = new System.Windows.Forms.Label();
+            this.txt_Resultado = new System.Windows.Forms.TextBox();
+            this.lbl_Nota_Completiva = new System.Windows.Forms.Label();
+            this.txt_Nota_Completiva = new System.Windows.Forms.TextBox();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.txt_Total = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_nota1
+            // 
+            this.txt_nota1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nota1.Location = new System.Drawing.Point(145, 40);
+            this.txt_nota1.Name = "txt_nota1";
+            this.txt_nota1.Size = new System.Drawing.Size(144, 30);
+            this.txt_nota1.TabIndex = 0;
+            // 
+            // lbl_n1
+            // 
+            this.lbl_n1.AutoSize = true;
+            this.lbl_n1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_n1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_n1.Location = new System.Drawing.Point(33, 45);
+            this.lbl_n1.Name = "lbl_n1";
+            this.lbl_n1.Size = new System.Drawing.Size(59, 22);
+            this.lbl_n1.TabIndex = 1;
+            this.lbl_n1.Text = "Nota1";
+            // 
+            // lbl_n2
+            // 
+            this.lbl_n2.AutoSize = true;
+            this.lbl_n2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_n2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_n2.Location = new System.Drawing.Point(33, 88);
+            this.lbl_n2.Name = "lbl_n2";
+            this.lbl_n2.Size = new System.Drawing.Size(59, 22);
+            this.lbl_n2.TabIndex = 3;
+            this.lbl_n2.Text = "Nota2";
+            // 
+            // txt_nota2
+            // 
+            this.txt_nota2.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nota2.Location = new System.Drawing.Point(145, 83);
+            this.txt_nota2.Name = "txt_nota2";
+            this.txt_nota2.Size = new System.Drawing.Size(144, 30);
+            this.txt_nota2.TabIndex = 2;
+            // 
+            // txt_nota3
+            // 
+            this.txt_nota3.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nota3.Location = new System.Drawing.Point(145, 130);
+            this.txt_nota3.Name = "txt_nota3";
+            this.txt_nota3.Size = new System.Drawing.Size(144, 30);
+            this.txt_nota3.TabIndex = 4;
+            // 
+            // lbl_n4
+            // 
+            this.lbl_n4.AutoSize = true;
+            this.lbl_n4.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_n4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_n4.Location = new System.Drawing.Point(33, 180);
+            this.lbl_n4.Name = "lbl_n4";
+            this.lbl_n4.Size = new System.Drawing.Size(59, 22);
+            this.lbl_n4.TabIndex = 7;
+            this.lbl_n4.Text = "Nota4";
+            // 
+            // txt_nota4
+            // 
+            this.txt_nota4.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nota4.Location = new System.Drawing.Point(145, 175);
+            this.txt_nota4.Name = "txt_nota4";
+            this.txt_nota4.Size = new System.Drawing.Size(144, 30);
+            this.txt_nota4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(33, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Promedio";
+            // 
+            // txt_promedio
+            // 
+            this.txt_promedio.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_promedio.Location = new System.Drawing.Point(145, 216);
+            this.txt_promedio.Name = "txt_promedio";
+            this.txt_promedio.Size = new System.Drawing.Size(144, 30);
+            this.txt_promedio.TabIndex = 8;
+            // 
+            // lbl_n3
+            // 
+            this.lbl_n3.AutoSize = true;
+            this.lbl_n3.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_n3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_n3.Location = new System.Drawing.Point(33, 136);
+            this.lbl_n3.Name = "lbl_n3";
+            this.lbl_n3.Size = new System.Drawing.Size(59, 22);
+            this.lbl_n3.TabIndex = 10;
+            this.lbl_n3.Text = "Nota3";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Calcular.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(94, 368);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(195, 66);
+            this.btn_Calcular.TabIndex = 11;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = false;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(359, 368);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(195, 66);
+            this.btn_Limpiar.TabIndex = 12;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Cerrar.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cerrar.Location = new System.Drawing.Point(655, 368);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(195, 66);
+            this.btn_Cerrar.TabIndex = 13;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // lbl_Situacion
+            // 
+            this.lbl_Situacion.AutoSize = true;
+            this.lbl_Situacion.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Situacion.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Situacion.Location = new System.Drawing.Point(35, 267);
+            this.lbl_Situacion.Name = "lbl_Situacion";
+            this.lbl_Situacion.Size = new System.Drawing.Size(87, 22);
+            this.lbl_Situacion.TabIndex = 15;
+            this.lbl_Situacion.Text = "Situacion";
+            // 
+            // txt_situacion
+            // 
+            this.txt_situacion.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_situacion.Location = new System.Drawing.Point(145, 264);
+            this.txt_situacion.Name = "txt_situacion";
+            this.txt_situacion.Size = new System.Drawing.Size(244, 30);
+            this.txt_situacion.TabIndex = 14;
+            // 
+            // lbl_Pruba_Completiva
+            // 
+            this.lbl_Pruba_Completiva.AutoSize = true;
+            this.lbl_Pruba_Completiva.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Pruba_Completiva.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Pruba_Completiva.Location = new System.Drawing.Point(463, 102);
+            this.lbl_Pruba_Completiva.Name = "lbl_Pruba_Completiva";
+            this.lbl_Pruba_Completiva.Size = new System.Drawing.Size(158, 22);
+            this.lbl_Pruba_Completiva.TabIndex = 17;
+            this.lbl_Pruba_Completiva.Text = "Pruba Completiva";
+            // 
+            // txt_Pruba_Completiva
+            // 
+            this.txt_Pruba_Completiva.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Pruba_Completiva.Location = new System.Drawing.Point(645, 94);
+            this.txt_Pruba_Completiva.Name = "txt_Pruba_Completiva";
+            this.txt_Pruba_Completiva.Size = new System.Drawing.Size(144, 30);
+            this.txt_Pruba_Completiva.TabIndex = 16;
+            this.txt_Pruba_Completiva.Text = "0";
+            // 
+            // lbl_Resultado
+            // 
+            this.lbl_Resultado.AutoSize = true;
+            this.lbl_Resultado.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Resultado.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Resultado.Location = new System.Drawing.Point(463, 204);
+            this.lbl_Resultado.Name = "lbl_Resultado";
+            this.lbl_Resultado.Size = new System.Drawing.Size(91, 22);
+            this.lbl_Resultado.TabIndex = 19;
+            this.lbl_Resultado.Text = "Resultado";
+            // 
+            // txt_Resultado
+            // 
+            this.txt_Resultado.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Resultado.Location = new System.Drawing.Point(645, 196);
+            this.txt_Resultado.Name = "txt_Resultado";
+            this.txt_Resultado.Size = new System.Drawing.Size(216, 30);
+            this.txt_Resultado.TabIndex = 18;
+            // 
+            // lbl_Nota_Completiva
+            // 
+            this.lbl_Nota_Completiva.AutoSize = true;
+            this.lbl_Nota_Completiva.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Nota_Completiva.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Nota_Completiva.Location = new System.Drawing.Point(463, 45);
+            this.lbl_Nota_Completiva.Name = "lbl_Nota_Completiva";
+            this.lbl_Nota_Completiva.Size = new System.Drawing.Size(147, 22);
+            this.lbl_Nota_Completiva.TabIndex = 21;
+            this.lbl_Nota_Completiva.Text = "Nota Completiva";
+            // 
+            // txt_Nota_Completiva
+            // 
+            this.txt_Nota_Completiva.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nota_Completiva.Location = new System.Drawing.Point(645, 45);
+            this.txt_Nota_Completiva.Name = "txt_Nota_Completiva";
+            this.txt_Nota_Completiva.Size = new System.Drawing.Size(144, 30);
+            this.txt_Nota_Completiva.TabIndex = 20;
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Total.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Total.Location = new System.Drawing.Point(463, 155);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(51, 22);
+            this.lbl_Total.TabIndex = 23;
+            this.lbl_Total.Text = "Total";
+            // 
+            // txt_Total
+            // 
+            this.txt_Total.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Total.Location = new System.Drawing.Point(645, 147);
+            this.txt_Total.Name = "txt_Total";
+            this.txt_Total.Size = new System.Drawing.Size(144, 30);
+            this.txt_Total.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(883, 537);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.txt_Total);
+            this.Controls.Add(this.lbl_Nota_Completiva);
+            this.Controls.Add(this.txt_Nota_Completiva);
+            this.Controls.Add(this.lbl_Resultado);
+            this.Controls.Add(this.txt_Resultado);
+            this.Controls.Add(this.lbl_Pruba_Completiva);
+            this.Controls.Add(this.txt_Pruba_Completiva);
+            this.Controls.Add(this.lbl_Situacion);
+            this.Controls.Add(this.txt_situacion);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.lbl_n3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_promedio);
+            this.Controls.Add(this.lbl_n4);
+            this.Controls.Add(this.txt_nota4);
+            this.Controls.Add(this.txt_nota3);
+            this.Controls.Add(this.lbl_n2);
+            this.Controls.Add(this.txt_nota2);
+            this.Controls.Add(this.lbl_n1);
+            this.Controls.Add(this.txt_nota1);
+            this.Name = "Form1";
+            this.Text = "Calcular Situacion De Alumno";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nota1;
+        private System.Windows.Forms.Label lbl_n1;
+        private System.Windows.Forms.Label lbl_n2;
+        private System.Windows.Forms.TextBox txt_nota2;
+        private System.Windows.Forms.TextBox txt_nota3;
+        private System.Windows.Forms.Label lbl_n4;
+        private System.Windows.Forms.TextBox txt_nota4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_promedio;
+        private System.Windows.Forms.Label lbl_n3;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.Label lbl_Situacion;
+        private System.Windows.Forms.TextBox txt_situacion;
+        private System.Windows.Forms.Label lbl_Pruba_Completiva;
+        private System.Windows.Forms.TextBox txt_Pruba_Completiva;
+        private System.Windows.Forms.Label lbl_Resultado;
+        private System.Windows.Forms.TextBox txt_Resultado;
+        private System.Windows.Forms.Label lbl_Nota_Completiva;
+        private System.Windows.Forms.TextBox txt_Nota_Completiva;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.TextBox txt_Total;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_ejercicio4_promedio_estudiante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            txt_Resultado.Visible = false;
+            lbl_Resultado.Visible=false;
+            txt_Pruba_Completiva.Visible = false;
+            lbl_Pruba_Completiva.Visible=false;
+            txt_Nota_Completiva.Visible = false;
+            lbl_Nota_Completiva.Visible = false;
+            txt_Total.Visible = false;
+            lbl_Total.Visible=false;
+            
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+
+            int Nota1, Nota2, Nota3, Nota4;
+
+
+
+            Nota1 = Convert.ToInt32(txt_nota1.Text);
+            Nota2 = Convert.ToInt32(txt_nota2.Text);
+            Nota3 = Convert.ToInt32(txt_nota3.Text);
+            Nota4 = Convert.ToInt32(txt_nota4.Text);
+
+            double Promedio = (Nota1 + Nota2 + Nota3 + Nota4) / 4;            
+
+            txt_promedio.Text = Promedio.ToString();
+
+
+
+
+            if (Promedio >= 70)
+            {
+                txt_situacion.Text = "Aprobado";
+                txt_situacion.ForeColor = Color.Green;
+                txt_promedio.ForeColor = Color.Green;
+
+
+            }
+
+            else if (Promedio <= 69)
+            {
+                txt_situacion.Text = "Reprobado";
+                txt_situacion.ForeColor = Color.Red;
+                txt_promedio.ForeColor = Color.Red;
+            }
+
+
+
+
+
+
+
+            if (Promedio <= 69)
+            {
+
+                txt_Resultado.Visible = true;
+                lbl_Resultado.Visible = true;
+                txt_Pruba_Completiva.Visible = true;
+                lbl_Pruba_Completiva.Visible = true;
+                txt_Nota_Completiva.Visible = true;
+                lbl_Nota_Completiva.Visible = true;
+                txt_Total.Visible = true;
+                lbl_Total.Visible = true;
+
+
+
+
+                txt_Resultado.Enabled = false;
+                txt_Nota_Completiva.Enabled = false;
+                txt_Total.Enabled = false;
+
+                double NotaCompletiva = Promedio * 50 / 100;
+                txt_Nota_Completiva.Text = NotaCompletiva.ToString();
+
+                int PruebaCompletiva = 0;
+                 PruebaCompletiva = int.Parse(txt_Pruba_Completiva.Text);
+
+
+                double Porciento_Prueba_Completiva = PruebaCompletiva * 50 / 100;
+
+
+                double total = NotaCompletiva + Porciento_Prueba_Completiva;
+
+                txt_Total.Text = total.ToString();
+
+                string resultado;
+
+                if(total >= 70)
+                {
+
+                    resultado = "Aprobado";
+                    txt_Resultado.Text = resultado;
+                    txt_Resultado.ForeColor = Color.Green;
+
+                }
+                else if (total <= 69)
+                {
+
+                    resultado = "Reprobado";
+                    txt_Resultado.Text = resultado;
+                    txt_Resultado.ForeColor = Color.Red;
+
+                }
+            }
+
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_nota1.Clear();
+            txt_nota2.Clear();
+            txt_nota3.Clear();
+            txt_nota4.Clear();
+            txt_promedio.Clear();
+            txt_situacion.Clear();
+
+            txt_Nota_Completiva.Clear();
+            txt_Pruba_Completiva.Clear();
+            txt_Resultado.Clear();
+            txt_Total.Clear();
+        }
+    }
+}
